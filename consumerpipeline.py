317b3eeef4c9d9d1d2ab6716c04ba078d4fd5d9d
anuraggdtc <anurag.pareek@godigitaltc.com>
2025-08-01 18:45:22 +0530

    Update consumerpipeline.py
    made changes and got it to work

--- consumerpipeline.py
+++ consumerpipeline.py
@@ -1,54 +1,56 @@
+import json
 from confluent_kafka import Consumer, KafkaException
-import logging
+import pandas as pd
+from pipeline import logger
 from sqlalchemy import create_engine
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
+def create_kafka_consumer(topic):
+    config = {
+    'bootstrap.servers': 'localhost:29092',
+    'group.id': 'earthquake-consumer',
+    'auto.offset.reset': 'latest',
+    }
+    consumer = Consumer(config)
+    consumer.subscribe([topic])
+    return consumer
 
-def create_kafka_consumer():
-    # this will give me the conf
-    conf = {
-            'bootstrap.servers': 'localhost:29092',
-            'group.id': 'my_consumer_group',
-            'auto.offset.reset': 'earliest'}
-    return Consumer(conf)
+def msg_to_df(cons, dataframe):
+    while True:
+        msg = cons.poll(10.0) # Poll for a longer timeout to wait for messages
+        # err ye empty
+        if msg is None:
+            print("No message till now.")
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
 
+        # main message
+        try:
+            consumed_data = json.loads(msg.value().decode('utf-8'))
+            #print(f"mila: {consumed_data}")
+            new_data_df = pd.DataFrame([consumed_data])
+            dataframe = pd.concat([dataframe, new_data_df], ignore_index=True)
+            print(dataframe) 
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            continue
+        return dataframe
+    
 def store_in_postgres(df):
-    # same as producerpipeline
     if df.empty:
         logger.info("No records to insert into PostgreSQL")
+        # return nahi toh gives error and stops 
         return
     db_connection_str = 'postgresql://root:root@localhost:5432/db'
     engine = create_engine(db_connection_str)
-    try:#just changed the table
+    try:
         df.to_sql('consumertable', engine, if_exists='append', index=False)
         logger.info(f"Inserted {len(df)} records into PostgreSQL")
-    except Exception as e: 
+    except Exception as e: # why is new data not showing? (fixd)
         logger.error(f"PostgreSQL insert error: {e}")
 
-def consume_data(consumer,mytopic):
-    consumer.subscribe([mytopic])
-    try:
-        while True:
-            msg = consumer.poll(timeout=1.0) # Poll for messages with a 1-second timeout
-            if msg is None:
-                print(msg)
-            if msg.error():
-                if msg.error().code() == KafkaException._PARTITION_EOF:
-                    # End of partition event
-                    print(f"End of partition reached {msg.topic()} [{msg.partition()}]")
-                else:
-                    print(f"Consumer error: {msg.error()}")
-            else:
-                # Process the message
-                consumed = msg.value().decode('utf-8')
-                return consumed 
-    except KeyboardInterrupt:
-        pass
-
-def main():
-    # get the conf
-    consumer_obj = create_kafka_consumer()
-    topic = 'earthquake_data'
-    consumed_data = consume_data(consumer_obj,topic)
-
+if __name__ == "__main__":
+    consumer_obj = create_kafka_consumer(topic='earthquake_data')
+    dff = pd.DataFrame()
+    df = msg_to_df(consumer_obj, dff)
